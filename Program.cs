9c2de72fa4568898a28b35f3227b05bdd5f59f71
Frostblazer <42157097+Frostblazer@users.noreply.github.com>
2020-05-28 19:26:46 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocialMediaFriends
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfFriends = new List<string>();
+
+            Console.WriteLine("Enter the names of your friends that liked your post.");
+            var userInput = Console.ReadLine();
+
+            while(!String.IsNullOrEmpty(userInput))
+            {
+                listOfFriends.Add(userInput);
+                Console.WriteLine("Enter another name.");
+                userInput = Console.ReadLine();
+            }
+
+            if(listOfFriends.Count == 1)
+            {
+                Console.WriteLine(listOfFriends[0]+" likes your post.");
+            }
+            else if(listOfFriends.Count == 2)
+            {
+                Console.WriteLine(listOfFriends[0] + " and "+ listOfFriends[1] + " like your post.");
+            }
+            else if(listOfFriends.Count > 2)
+            {
+                Console.WriteLine(listOfFriends[0] + ", " + listOfFriends[1] + " and " + (listOfFriends.Count-2) +" others like your post.");
+            }
+            else
+            {
+                return;
+            }
+
+        }
+    }
+}
